643aa1b0e7e3275a2008fee2978cb3245248d95a
Adrian Lewkowicz <adrian.lewkowiczm@gmail.com>
2025-02-26 19:00:49 +0100

    Modyfikowanie

--- src/app/features/employees/employees.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { EmployeesModel } from './employees.model';
-
-describe('EmployeesModel', () => {
-  it('should create an instance', () => {
-    expect(new EmployeesModel()).toBeTruthy();
-  });
-});
--- src/app/features/tasks/tasks.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TasksModel } from './tasks.model';
-
-describe('TasksModel', () => {
-  it('should create an instance', () => {
-    expect(new TasksModel()).toBeTruthy();
-  });
-});
